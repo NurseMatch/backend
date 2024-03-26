4b044f46ef375f29201a59f5f5f5b3bcd2f1efc6
douglashammarstam <dogge20@hotmail.com>
2024-03-26 19:56:40 +0100

    Revert "Add assignment endpoints and remove location from Assignment struct"
    This reverts commit 1d460c53462d08bc8d4968a48c3145d379a61663.

--- data/assignment.go
+++ data/assignment.go
@@ -6,6 +6,8 @@ import (
 
 type Assignment struct {
 	gorm.Model
+	LocationID      uint
+	Location        Location `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Shift           string
 	HourlySalarySek int
 }
